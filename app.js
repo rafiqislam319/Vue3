8121b622809ab53e66ba609f96ae27989a92af36
rafiqislam319 <rafiqislam319@gmail.com>
2022-06-30 23:13:58 +0600

    create properties in data function and outputting them in template

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let app = Vue.createApp({
-  //
+  data() {
+    return {
+      title: "The Net Ninja Vue Course",
+      author: "Shaun",
+      duration: "4hours",
+    }
+  },
 
 })
 app.mount('#app')
